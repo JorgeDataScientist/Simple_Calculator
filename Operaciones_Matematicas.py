d64219bad5918dd0f9f748548e7eaccd9a7ea073
Jorge Luis <117787883+BurroFlautista@users.noreply.github.com>
2022-11-17 20:04:38 -0500

    Add files via upload

---
+++ Operaciones_Matematicas.py
@@ -0,0 +1,12 @@
+#suma
+def add(n1,n2):
+    return n1+n2
+#resta
+def subtract(n1,n2):
+    return n1-n2
+#multiplica
+def multiply(n1,n2):
+    return n1*n2
+#divide
+def divide(n1,n2):
+    return n1/n2
